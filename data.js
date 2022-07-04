0f88a373e347c51ce70740469e72bec498f702f7
Bharatsharma77 <bharatsharma9368@gmail.com>
2022-07-04 14:49:05 +0530

    ccreate DOM ELEMENTS

---
+++ data.js
@@ -0,0 +1,50 @@
+let movies = [
+    {
+
+     name: 'falcon and winter soldier',
+     des: 'Generate Lorem Ipsum placeholder text. Select the number of characters, words, sentences or paragraphs, and hit generate!',
+     image: 'images/slider 2.PNG'
+
+    },
+
+
+    {
+
+        name: 'loki',
+        des: 'Generate Lorem Ipsum placeholder text. Select the number of characters, words, sentences or paragraphs, and hit generate!',
+        image: 'images/slider 1.PNG'
+   
+    },
+
+
+     {
+
+        name: 'wanda vision',
+        des: 'Generate Lorem Ipsum placeholder text. Select the number of characters, words, sentences or paragraphs, and hit generate!',
+        image: 'images/slider 3.PNG'
+   
+     },
+
+
+    {
+
+        name: 'raya and the last dragon',
+        des: 'Generate Lorem Ipsum placeholder text. Select the number of characters, words, sentences or paragraphs, and hit generate!',
+        image: 'images/slider 4.PNG'
+   
+   
+   
+    },
+
+
+
+       {
+
+        name: 'luca',
+        des: 'Generate Lorem Ipsum placeholder text. Select the number of characters, words, sentences or paragraphs, and hit generate!',
+        image: 'images/slider 5.PNG'
+   
+   
+   
+       }
+]
